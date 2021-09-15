5eedc1c85e8dc3812fb72764a8cc960dc905699c
jayko <jayko95@naver.com>
2021-09-15 16:00:30 +0900

    Add: Contents List endpoint 컨텐츠 리스트 반환 리스트 반환 조건 부여 조건 : Ordering, Limit, Category, Nation, Gerne if 최소화 사용 및 객체 조건 oneline

--- contents/urls.py
+++ contents/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ContentView
+from .views import ContentView, ContentListView
 
 urlpatterns = [
     path("/<int:content_id>", ContentView.as_view()),
+    path("/list", ContentListView.as_view()),
 ]
