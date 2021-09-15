a101ecfeb88c9f4ddca4ba20425d4e6f75efd067
jayko <jayko95@naver.com>
2021-09-15 14:22:36 +0900

    Add: Content Detail info endpoint 컨텐츠의 기본 정보와 컨텐츠가 소유한 episode 정보 return

--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Content
+
+class ContentView(View):
+    def get(self, request, content_id):
+        try:
+            content = Content.objects.prefetch_related("genre", "details").get(id=content_id)
+            result  = {
+                "id"          : content.id,
+                "name"        : content.name,
+                "category"    : content.category,
+                "description" : content.description,
+                "nation"      : content.nation,
+                "thumb_nail"  : content.thumb_nail,
+                "genre"       : [{"genre" : genre.name} for genre in content.genre.all()],
+                "detail"      : [{
+                    "episode"            : detail.episode,
+                    "detail_description" : detail.description,
+                    "running_time"       : detail.running_time,
+                    "detail_thumb_nail"  : detail.thumb_nail,
+                    } for detail in content.details.all()]
+            }
+
+            return JsonResponse({"Result": result}, status=200)
+
+        except Content.DoesNotExist:
+            return JsonResponse({"Result": "CONTENT_DOES_NOT_EXIST"}, status=404)
