12f0eea2102ff9981bd01195ac3a5452973df8e2
Hope Kim <kzang2028@gmail.com>
2021-09-16 10:20:28 +0900

    Add: added basic login view - Add: added decorator(on going) - Add: added list of unit test todos - Add: added test cases & organized views - Add & Mod: added sign-in test cases & modified sigin-in views - Mod: created tests folder & moved tests.py into the folder - Mod: modified token validation function to decorator & set expire time to 1 min for test

--- users/views.py
+++ users/views.py
@@ -1,12 +1,16 @@
 import json
 import re
 import bcrypt
+import jwt
+from datetime import datetime, timedelta
 
 from django.views import View
 from django.http import JsonResponse
 from django.db.utils import DataError
 
 from users.models import User
+from my_settings import SECRET_KEY, ALGORITHM
+from users.login_decorator import token_validation_decorator
 
 
 class SignUpView(View):
@@ -19,7 +23,7 @@ class SignUpView(View):
             if not email_format.search(data['email']):
                 return JsonResponse({'message': 'INVALID_EMAIL_FORMAT'}, status=400)
             if not password_format.match(data['password']):
-                return JsonResponse({'message': 'INVALID_PASSWORD_FORMAT'})
+                return JsonResponse({'message': 'INVALID_PASSWORD_FORMAT'}, status=400)
 
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message': 'USER_ALREADY_EXISTS'}, status=400)
@@ -38,6 +42,33 @@ class SignUpView(View):
             return JsonResponse({'message': 'SUCCESS'}, status=201)
 
         except KeyError:
-            return JsonResponse({'message': 'KEYERROR'}, status=400)
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
         except DataError:
-            return JsonResponse({'message': 'DATA_TOO_LONG'})
+            return JsonResponse({'message': 'DATA_TOO_LONG'}, status=400)
+
+
+class SignInView(View):
+    def post(self, request):
+        ENCODE_FORMAT = 'utf-8'
+        algorithm     = ALGORITHM
+        data          = json.loads(request.body)
+
+        try:
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message': 'USER_DOES_NOT_EXIST'}, status=401)
+            
+            user = User.objects.get(email=data['email'])
+            if not bcrypt.checkpw(data['password'].encode(ENCODE_FORMAT), user.password.encode(ENCODE_FORMAT)):
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
+
+            access_token = jwt.encode({
+                'user_id': user.id,
+                'exp'    : datetime.utcnow() + timedelta(minutes=1)
+                }, SECRET_KEY, algorithm=algorithm)
+            return JsonResponse({
+                'message': 'LOGIN_SUCCESS', 
+                'token': access_token
+                }, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
