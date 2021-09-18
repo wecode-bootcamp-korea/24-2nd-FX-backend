3cd30b1ddcff3bc9ddfe9fba95946aaedc6e9359
jayko <jayko95@naver.com>
2021-09-18 16:22:21 +0900

    Add: Togglewishlist

--- wishlists/models.py
+++ wishlists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from core.models import TimeStampedModel
 
 class Wishlist(TimeStampedModel):
-    like        = models.BooleanField(default=False)
+    like        = models.BooleanField(default=True)
     user        = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="wishlists")
     content     = models.ForeignKey("contents.Content", on_delete=models.CASCADE, related_name="wishlists")
 
