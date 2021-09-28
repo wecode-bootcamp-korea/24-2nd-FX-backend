12edb16a2f0d5df5731bcdcf34dc2d94d7b1c0a7
Hope Kim <kzang2028@gmail.com>
2021-09-28 16:44:53 +0900

    Add: added google login - Add: added success test case - Add: added key error test case

--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
 import json
 import re
-import bcrypt
 import jwt
-from enum import Enum, unique, auto
+import bcrypt
 from datetime import datetime, timedelta
 
 from django.views import View
@@ -11,7 +10,6 @@ from django.db.utils import DataError
 
 from users.models import User
 from my_settings import SECRET_KEY, ALGORITHM
-from users.login_decorator import token_validation_decorator
 
 
 class SignUpView(View):
@@ -75,3 +73,38 @@ class SignInView(View):
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+
+class GoogleLogInView(View):
+    def post(self, request):
+
+        try:
+            user = request.headers.get('Authorization')
+            data = json.loads(request.body)
+
+            if not user:
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            User.objects.update_or_create(
+                kakao_id    = data['google_id'],
+                signup_type = 'GOOGLE',
+                password    = '',
+                defaults    = {'name': data['name']}
+            )
+  
+            user_info = User.objects.get(kakao_id=data['google_id'])
+
+            flix_access_token = jwt.encode({
+                'user_id'    : user_info.id,
+                'login_type' : 'GOOGLE',
+                'exp'        : datetime.utcnow() + timedelta(minutes=1),
+                }, SECRET_KEY, algorithm=ALGORITHM)
+
+            return JsonResponse({
+                'message': 'LOGIN_SUCCESS',
+                'token'  : flix_access_token
+                }, status=201)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        
